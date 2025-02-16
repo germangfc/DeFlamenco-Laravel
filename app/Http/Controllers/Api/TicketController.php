0fb395c03b2e97bc14dc86460880d500ed3d0f3e
Samuel Cortés Sánchez <samuelcortessanchez@gmail.com>
2025-02-16 17:47:25 +0100

    Optimizacion del controlador y creacion de seeders de ticket

--- app/Http/Controllers/Api/TicketController.php
+++ app/Http/Controllers/Api/TicketController.php
@@ -8,40 +8,43 @@ use Illuminate\Http\Request;
 
 class TicketController extends Controller
 {
-    public function index(){
-        return response()->json(Ticket::all(),200);
+    public function index()
+    {
+        return response()->json(Ticket::all(), 200);
     }
 
-    public function show($id){
+    public function show($id)
+    {
         $ticket = Ticket::find($id);
-        if(!$ticket){
-            return response()->json(['message' => 'Ticket not found'],404);
+        if (!$ticket) {
+            return response()->json(['message' => 'Ticket not found'], 404);
         }
-        return response()->json($ticket,200);
+        return response()->json($ticket, 200);
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         $request->validate([
-            'idEvent'=>"required",
+            'idEvent' => 'required',
             'idClient' => 'required',
-            'price' => 'required',
+            'price' => 'required|numeric|min:0',
             'isReturned' => 'boolean'
         ]);
 
-        $ticket = Ticket::create($request->all(['idEvent','idClient','price','isReturned']));
+        $ticket = Ticket::create($request->only(['idEvent', 'idClient', 'price', 'isReturned']));
 
-        return response()->json($ticket,201);
+        return response()->json($ticket, 201);
     }
 
-    public function delete(Request $request, $id)
+    public function destroy($id)
     {
         $ticket = Ticket::find($id);
         if (!$ticket) {
             return response()->json(['message' => 'Ticket not found'], 404);
         }
 
-        $ticket->update($request->all(['isReturned']));
+        $ticket->update(['isReturned' => true]);
 
-        return response()->json($ticket, 200);
+        return response()->json(['ticket' => $ticket], 200);
     }
 }
