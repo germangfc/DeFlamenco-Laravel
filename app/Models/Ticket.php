68c264b1b307b742ada9cd87b5f8f31694a419ec
Álvaro Herero <alvaro.herrero11x@gmail.com>
2025-02-16 22:53:23 +0100

    Tcikets con factory

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use MongoDB\Laravel\Eloquent\Model as Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Ticket extends Model
 {
+    use HasFactory;
     protected $connection ='mongodb';
     protected $fillable = [
         "idEvent",
