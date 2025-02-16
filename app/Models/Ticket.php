1ba794527a8961e61f2d7377c32f77f3a6dca199
Álvaro Herero <alvaro.herrero11x@gmail.com>
2025-02-16 22:48:32 +0100

    mongo funcionando

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
-use MongoDB\Laravel\Eloquent\Model;
+use MongoDB\Laravel\Eloquent\Model as Model;
 
 class Ticket extends Model
 {
+    protected $connection ='mongodb';
     protected $fillable = [
         "idEvent",
         "idClient",
