da20e55d807e18176e75fca364d2e182f2a218be
Álvaro Herero <alvaro.herrero11x@gmail.com>
2025-02-21 09:11:48 +0100

    slider separado a componente

--- resources/views/card.blade.php
+++ resources/views/card.blade.php
@@ -2,14 +2,7 @@
 
 @section("content")
     <div class="p-8">
-        <div class="carousel w-full mb-8 relative overflow-hidden" id="slider">
-            <div class="carousel-inner flex transition-transform duration-1000 ease-in-out">
-                <img src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp" class="w-full flex-shrink-0" />
-                <img src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp" class="w-full flex-shrink-0" />
-                <img src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp" class="w-full flex-shrink-0" />
-                <img src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp" class="w-full flex-shrink-0" />
-            </div>
-        </div>
+        @include('components.slider')
 
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
             @foreach($eventos as $evento)
---
+++ resources/views/components/slider.blade.php
@@ -0,0 +1,8 @@
+<div class="carousel w-full mb-8 relative overflow-hidden" id="slider">
+    <div class="carousel-inner flex transition-transform duration-1000 ease-in-out">
+        <img src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp" class="w-full flex-shrink-0" />
+        <img src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp" class="w-full flex-shrink-0" />
+        <img src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp" class="w-full flex-shrink-0" />
+        <img src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp" class="w-full flex-shrink-0" />
+    </div>
+</div>
