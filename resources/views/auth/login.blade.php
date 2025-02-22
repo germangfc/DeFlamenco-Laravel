df3fbc2198584a0dcfa51346401bcadb34cb05f4
Álvaro Herero <alvaro.herrero11x@gmail.com>
2025-02-22 17:46:04 +0100

    tabla clientes fixeada, table basico añadido

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,15 +2,11 @@
 
     <form method="POST" action="{{ route('login') }}">
         @csrf
-
-        <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
-
-        <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
 
@@ -21,8 +17,6 @@
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
-
-        <!-- Remember Me -->
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded shadow-sm " name="remember">
