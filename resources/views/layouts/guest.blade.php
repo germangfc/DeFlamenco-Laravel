686ef7c143101712d94cbce9358bcc8334bb7e82
Álvaro Herero <alvaro.herrero11x@gmail.com>
2025-02-22 10:32:08 +0100

    cambio colores de tema + componentes

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,20 +11,41 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans  antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 ">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
-
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-base-400  shadow-md overflow-hidden sm:rounded-lg">
+    <body class="font-sans antialiased">
+    <div class="min-h-screen flex flex-col md:flex-row">
+        <!-- Lado superior para pantallas pequeñas y lateral para pantallas grandes: Imagen -->
+        <div class="w-full md:w-1/2 flex items-center justify-center bg-cover bg-center" style="background-image: url('{{ Vite::asset("resources/images/foto_login_flamenco_con_fondo.svg") }}');">
+            <!-- Puedes agregar contenido extra si lo deseas -->
+        </div>
+        <!-- Lado inferior para pantallas pequeñas y lateral para pantallas grandes: Formulario de login -->
+        <div class="flex flex-col w-full md:w-1/2 justify-center items-center bg-base-100">
+            <h2 class="text-3xl">Bienvenido a Tablao Pass</h2>
+            <div class="w-full sm:max-w-md mt-6 bg-base-200 px-6 py-4 shadow-2xl overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
         </div>
+    </div>
     </body>
+
+
 </html>
+
+<script>
+    document.addEventListener("DOMContentLoaded", function () {
+        const themeToggle = document.getElementById("theme-toggle");
+        const currentTheme = localStorage.getItem("theme") || "flamencoLight"; // Tema por defecto
+
+        document.documentElement.setAttribute("data-theme", currentTheme);
+        themeToggle.checked = currentTheme === "flamencoDark"; // Activa el checkbox si el tema es oscuro
+
+        themeToggle.addEventListener("change", function () {
+            const newTheme = themeToggle.checked ? "flamencoDark" : "flamencoLight";
+            document.documentElement.setAttribute("data-theme", newTheme);
+            localStorage.setItem("theme", newTheme);
+        });
+    });
+</script>
