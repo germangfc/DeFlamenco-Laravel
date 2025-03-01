9580187ba547118ec25256cfc645147d2758bfe2
germanGFC <147338370+germangfc@users.noreply.github.com>
2025-03-01 18:19:15 +0100

    ventas index y show views remodeling
    added creation date and rest of fields for detail show

--- resources/views/ventas/index.blade.php
+++ resources/views/ventas/index.blade.php
@@ -5,79 +5,87 @@
     <div class="container mx-auto mt-8">
         <h1 class="text-3xl font-bold text-center mb-8">Listado de Ventas</h1>
 
-        @foreach($ventas as $venta)
-            <div class="mb-8 font-marcellus">
-                <!-- Encabezado de la venta -->
-                <h4 class="text-2xl font-semibold mb-4">Venta: {{ $venta->guid }}</h4>
-                <div class="card bg-base-100 shadow-xl">
-                    <div class="card-body">
-                        <div class="overflow-x-auto">
-                            <table class="table w-full">
-                                <thead>
-                                <tr class="font-black">
-                                    <th>Id Ticket</th>
-                                    <th>Precio</th>
-                                    <th>Evento</th>
-                                    <th>Fecha</th>
-                                    <th>Hora</th>
-                                    <th>Ciudad</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                @php
-                                    $total = 0;
-                                @endphp
-                                @foreach($venta->lineasVenta as $lineaVenta)
-                                    @php
-                                        // Se asume que el índice 1 es el precio
-                                        $total += $lineaVenta[1];
-                                    @endphp
-                                    <tr>
-                                        <td>{{ $lineaVenta[0] }}</td>
-                                        <td>{{ number_format($lineaVenta[1],2, ',', '.') }} €</td>
-                                        <td>{{ $lineaVenta[2] }}</td>
-                                        <td>{{ Carbon::parse($lineaVenta[3])->format('d-m-Y') }}</td>
-                                        <td>{{ Carbon::parse($lineaVenta[4])->format('H:i:s') }}</td>
-                                        <td>{{ $lineaVenta[5] }}</td>
+        @if(empty($ventas))
+            <p class="text-center text-gray-600">No hay ventas para mostrar.</p>
+        @else
+            @foreach($ventas as $venta)
+                <div class="mb-8 font-marcellus">
+                    <!-- Encabezado de la venta -->
+                    <div class="flex items-center justify-between mb-4">
+                        <h4 class="text-2xl font-semibold">Venta: {{ $venta->guid }}</h4>
+                        <p class="text-gray-600 text-base">Fecha: {{ Carbon::parse($venta->created_at)->format('d-m-Y') }}</p>
+                    </div>
+                    <div class="card bg-base-100 shadow-xl">
+                        <div class="card-body">
+                            <div class="overflow-x-auto">
+                                <table class="table w-full">
+                                    <thead>
+                                    <tr class="font-black">
+                                        <th>Id Ticket</th>
+                                        <th>Precio</th>
+                                        <th>Evento</th>
+                                        <th>Fecha</th>
+                                        <th>Hora</th>
+                                        <th>Ciudad</th>
                                     </tr>
-                                @endforeach
-                                <!-- Fila de total y botón "Detalle" -->
-                                <tr class="bg-customBeige  font-bold">
-                                    <td colspan=5" class="text-right pr-4">Total: {{ number_format($total, 2, ',', '.') }} €</td>
-                                    <td>
-                                        <a href="{{ route('ventas.show', $venta->id) }}" class="btn btn-info btn-sm">
-                                            Detalle
-                                        </a>
-                                    </td>
-                                </tr>
-                                </tbody>
-                            </table>
+                                    </thead>
+                                    <tbody>
+                                    @php
+                                        $total = 0;
+                                    @endphp
+                                    @foreach($venta->lineasVenta as $lineaVenta)
+                                        @php
+                                            // Se asume que el índice 1 es el precio
+                                            $total += $lineaVenta[1];
+                                        @endphp
+                                        <tr>
+                                            <td>{{ $lineaVenta[0] }}</td>
+                                            <td>{{ number_format($lineaVenta[1],2, ',', '.') }} €</td>
+                                            <td>{{ $lineaVenta[2] }}</td>
+                                            <td>{{ Carbon::parse($lineaVenta[3])->format('d-m-Y') }}</td>
+                                            <td>{{ Carbon::parse($lineaVenta[4])->format('H:i:s') }}</td>
+                                            <td>{{ $lineaVenta[5] }}</td>
+                                        </tr>
+                                    @endforeach
+                                    <!-- Fila de total y botón "Detalle" -->
+                                    <tr class="bg-customBeige  font-bold">
+                                        <td colspan=5" class="text-right pr-4">Total: {{ number_format($total, 2, ',', '.') }} €</td>
+                                        <td>
+                                            <a href="{{ route('ventas.show', $venta->id) }}" class="btn btn-info btn-sm">
+                                                Detalle
+                                            </a>
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        @endforeach
-
-        <!-- Paginación general de ventas -->
-        <div class="flex justify-center mt-8 mb-8 space-x-2">
-            @if ($ventas->onFirstPage())
-                <span class="btn btn-disabled">«</span>
-            @else
-                <a href="{{ $ventas->previousPageUrl() }}" class="btn btn-square">«</a>
-            @endif
-
-            @foreach ($ventas->getUrlRange(1, $ventas->lastPage()) as $page => $url)
-                <a href="{{ $url }}" class="btn btn-square {{ $page == $ventas->currentPage() ? 'btn-active' : '' }}">
-                    {{ $page }}
-                </a>
             @endforeach
 
-            @if ($ventas->hasMorePages())
-                <a href="{{ $ventas->nextPageUrl() }}" class="btn btn-square">»</a>
-            @else
-                <span class="btn btn-disabled">»</span>
-            @endif
-        </div>
+            <!-- Paginación general de ventas -->
+            <div class="flex justify-center mt-8 mb-8 space-x-2">
+                @if ($ventas->onFirstPage())
+                    <span class="btn btn-disabled">«</span>
+                @else
+                    <a href="{{ $ventas->previousPageUrl() }}" class="btn btn-square">«</a>
+                @endif
+
+                @foreach ($ventas->getUrlRange(1, $ventas->lastPage()) as $page => $url)
+                    <a href="{{ $url }}" class="btn btn-square {{ $page == $ventas->currentPage() ? 'btn-active' : '' }}">
+                        {{ $page }}
+                    </a>
+                @endforeach
+
+                @if ($ventas->hasMorePages())
+                    <a href="{{ $ventas->nextPageUrl() }}" class="btn btn-square">»</a>
+                @else
+                    <span class="btn btn-disabled">»</span>
+                @endif
+            </div>
+        @endif
+
     </div>
 @endsection
 
